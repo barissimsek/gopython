f2f2fad6a556f0a7c8e8cd84cd1b548bf1411711
Baris Simsek <simsek@thincube.co>
2017-09-30 16:28:59 -0700

    random file mixer

---
+++ mixer.py
@@ -0,0 +1,25 @@
+import random
+
+'''
+Random file mixer
+
+This small piece of code mixes lines randomly from a given file.
+It's important to have random dataset to train your machine learning algorithm.
+But mostly the data comes from structured sources so that it is may not be random.
+
+It reads data from a file named dataset.csv and prints on stdout.
+If needed redirect stdout to a file to have the random data as a file.
+
+# python mixer.py > mixed.csv
+
+-simsek
+'''
+
+with open('dataset.csv','r') as source:
+	data = [ (random.random(), line) for line in source ]
+
+data.sort()
+
+for line in data:
+	print line.strip()
+
