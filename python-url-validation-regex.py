17371a3a506ecd5779ec3478bc6ae6a2cd8fb8ba
Baris Simsek <simsek@thincube.co>
2017-10-06 00:28:13 -0700

    some improvements

--- python-url-validation-regex.py
+++ python-url-validation-regex.py
@@ -5,8 +5,8 @@ HTTP URL validator
 
 You can add other protocols by modifiying schema.
 HTTP paths may include almost anything. So I used (.) as the regular expression.
-If you want to limit it enable the previous commented line.
-You can see some new weird TLDs. Add them to the oTLDS.
+You can imit it by enabling the previous commented line.
+You can see some new weird TLDs. Add them to the oTLDs.
 
 Just copy the validateURL function and feed it with your urls.
 The main is to give you an example.
@@ -15,23 +15,26 @@ The main is to give you an example.
 
 '''
 
-def validateUrl(url):
-	schema = "http(s)?:\/\/"
-	subdomain = "[a-zA-Z0-9_-]+\."
-	tTLDs = "com|net|org|edu|int|gov|mil"
+def isValidUrl(url):
+	schema = "(http(s)?:\/\/)?"
+	subdomain = "([a-zA-Z0-9_-]+\.)+"
+	tTLDs = "(com|net|org|edu|int|gov|mil)"
 	cTLDs = "ac|ad|ae|af|ag|ai|al|am|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw"
 	oTLDs = "info|news"
 	TLDs = tTLDs + "|" + cTLDs + "|" + oTLDs
-	IP_addr = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-	port = ":[1-9][0-9]*"
-	#path = "/([a-zA-Z0-9\?=_\-\.%&#\:;,\/\+@\~\(\)!\$ ])*"
-	path = "/(.)*"
-	URL_REGEX = "^(" + schema + ")?((" + subdomain + ")+(" + TLDs + ")|(" + IP_addr + ")|(localhost))(" + port + ")?/?(" + path + ")*$"
+	IP_addr = "(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+	localhost = "(localhost)"
+	port = "(:[1-9][0-9]*)?"
+	#SPECIAL_URL_CHARS = "\?=_\-\.%&#\:;,\/\+@\~\(\)!\$ "
+	#path = "/([a-zA-Z0-9" + SPECIAL_URL_CHARS + "])*"
+	path = "(/(.)*)*"
+	
+	URL_REGEX = "^" + schema + "(" + subdomain + TLDs + "|" + IP_addr + "|" + localhost + ")" + port + path + "$"
 
 	if re.search(URL_REGEX, url):
-		return 1 # valid
+		return 1
 	else:
-		return 0 # not valid
+		return 0
 
 if __name__ == "__main__":
 
