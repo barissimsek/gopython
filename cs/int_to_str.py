0deee3df9a941cb07631742032142fa35ac19532
Baris Simsek <simsek@thincube.co>
2018-06-02 02:45:00 -0700

    int to str

---
+++ cs/int_to_str.py
@@ -0,0 +1,22 @@
+def int_to_str(i):
+	digits = '0123456789'
+	
+	if i == 0: 
+		return '0'
+
+	result = ''
+
+	while i > 0:
+		result = digits[i%10] + result
+		i = i // 10
+
+	return result
+
+if __name__== "__main__":
+
+	print(int_to_str(0))
+
+	print(int_to_str(8))
+
+	print(int_to_str(1807953))
+
